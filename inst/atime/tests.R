e9f344dc341386cc16ff539b8d4a4a04b479d738
Toby Dylan Hocking <tdhock5@gmail.com>
2023-01-24 23:22:21 -0700

    tests filel

---
+++ inst/atime/tests.R
@@ -0,0 +1,10 @@
+test.list <- atime::atime_grid(
+  list(DIST=c("mean_norm", "poisson", "laplace")),
+  test=list(
+    N=2^seq(2, 20),
+    setup={
+      max.segs <- as.integer(N/2)
+      data.vec <- 1:N
+    },
+    expr=binsegRcpp::binseg(DIST, data.vec, max.segs)))
+
