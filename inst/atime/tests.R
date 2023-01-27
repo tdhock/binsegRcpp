b7f889d11dc9363b049cb504693d858ed357acdb
Toby Dylan Hocking <tdhock5@gmail.com>
2023-01-26 21:07:59 -0700

    new name

--- inst/atime/tests.R
+++ inst/atime/tests.R
@@ -1,6 +1,6 @@
 test.list <- atime::atime_grid(
   list(DIST=c("mean_norm", "poisson", "meanvar_norm", "l1")),
-  test=list(
+  "binseg(1:N,maxSegs=N/2)"=list(
     N=2^seq(2, 20),
     setup={
       max.segs <- as.integer(N/2)
