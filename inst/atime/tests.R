5e5e1349304238b6d20b55dff236989caafec239
Toby Dylan Hocking <tdhock5@gmail.com>
2023-01-27 07:24:32 -0700

    each item in test list is a list of args

--- inst/atime/tests.R
+++ inst/atime/tests.R
@@ -1,20 +1,14 @@
-test.list <- c(
-  atime::atime_grid(
-    list(DIST=c("l1", "poisson", "meanvar_norm")),
-    "binseg(1:N,maxSegs=N/2)"=list(
-      N=2^seq(2, 20),
-      setup={
-        max.segs <- as.integer(N/2)
-        data.vec <- 1:N
-      },
-      expr=binsegRcpp::binseg(DIST, data.vec, max.segs))),
-  atime::atime_grid(
-    "binseg_normal(1:N,maxSegs=N/2)"=list(
-      seconds.limit=0.1,
-      N=2^seq(2, 20),
-      setup={
-        max.segs <- as.integer(N/2)
-        data.vec <- 1:N
-      },
-      expr=binsegRcpp::binseg_normal(data.vec, max.segs))))
-
+N.setup <- atime::atime_grid(
+  N=2^seq(2, 20),
+  setup={
+    max.segs <- as.integer(N/2)
+    data.vec <- 1:N
+  })
+expr.list <- atime::atime_grid(
+  list(DIST=c("l1", "poisson", "meanvar_norm")),
+  "binseg(1:N,maxSegs=N/2)"=binsegRcpp::binseg(DIST, data.vec, max.segs))
+partial.list <- lapply(expr.list, function(expr)list(expr=expr))
+partial.list[["binseg_normal(1:N,maxSegs=N/2)"]] <- list(
+  expr=quote(binsegRcpp::binseg_normal(data.vec, max.segs)),
+  seconds.limit=0.1) 
+test.list <- lapply(partial.list, function(plist)c(plist, N.setup))
