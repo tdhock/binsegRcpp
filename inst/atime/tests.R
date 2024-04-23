866af59965fec61a3b03bb2122d2dee35b6d6d87
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2024-04-23 14:34:20 -0700

    atime_test and list

--- inst/atime/tests.R
+++ inst/atime/tests.R
@@ -1,15 +1,19 @@
-N.setup <- atime::atime_grid(
+## three tests which are the same except for DIST arg.
+partial.list <- lapply(atime::atime_grid(
+  list(DIST=c("l1", "poisson", "meanvar_norm")),
+  "binseg(1:N,maxSegs=N/2)"=atime::atime_test(
+    expr=binsegRcpp::binseg(DIST, data.vec, max.segs))),
+  eval)
+test.list <- atime::atime_test_list(
   N=2^seq(2, 20),
   setup={
     max.segs <- as.integer(N/2)
     data.vec <- 1:N
-  })
-expr.list <- atime::atime_grid(
-  list(DIST=c("l1", "poisson", "meanvar_norm")),
-  "binseg(1:N,maxSegs=N/2)"=binsegRcpp::binseg(DIST, data.vec, max.segs))
-partial.list <- lapply(expr.list, function(expr)list(expr=expr))
-partial.list[["binseg_normal(1:N,maxSegs=N/2)"]] <- list(
-  expr=quote(binsegRcpp::binseg_normal(data.vec, max.segs)),
-  seconds.limit=0.1,
-  Before="083c6827aeae00568e31614bbec9bbb31dca1081")
-test.list <- lapply(partial.list, function(plist)c(plist, N.setup))
+  },
+  tests=partial.list,
+  ## A test using a different expr, limit, historical commit.
+  "binseg_normal(1:N,maxSegs=N/2)"=atime::atime_test(
+    expr=binsegRcpp::binseg_normal(data.vec, max.segs),
+    seconds.limit=0.1,
+    Before="083c6827aeae00568e31614bbec9bbb31dca1081"),
+  NULL)
